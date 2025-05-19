5c26be2f1bff373d9d12e391c80a57488463620d
rdefoundoux <rdefoundoux@erdv.ch>
2025-05-19 15:52:23 -0400

    Fix otp

--- otp-service/src/routes/otpRoutes.ts
+++ otp-service/src/routes/otpRoutes.ts
@@ -36,7 +36,15 @@ router.post('/otp/verify', async (req, res) => {
     console.log("otp",otp?.toString().trim());
     console.log("email",email);
 
-    await otpController.verifyOtp(email, otp?.toString().trim());
+    // Add these checks
+    if (!otp || typeof otp !== 'string') {
+      throw new Error('OTP is required');
+    }
+
+
+    const verificationResult = await otpController.verifyOtp(email, otp.trim());
+    console.log("verificationResult", verificationResult); // Log returned true
+
     console.log("otp verified");
     res.status(200).json({ message: 'OTP is verified' });
   } catch (error) {
